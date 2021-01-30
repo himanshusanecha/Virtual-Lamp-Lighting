bfc60b73af98f17e4005665b4182e1a9b9d05aff
Himanshu Sanecha <52252631+himanshusanecha@users.noreply.github.com>
2021-01-31 00:20:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var x = document.getElementsByClassName("over-img")[0];
+    var y = document.getElementsByClassName("over-img2")[0];
+    var z = document.getElementsByClassName("over-img3")[0];
+    var a = document.getElementsByClassName("over-img4")[0];
+    //console.log(x.style.display); 
+      x.style.display = "block";
+      y.style.display = "block";
+      a.style.display = "block";
+      z.style.display = "block";
+  }
